dc720a12019384451d5208b6ddf3dd20d5d99465
xushuai <466911254@qq.com>
2022-07-29 14:33:36 +0800

    simple test

--- simple/src/main/java/com/github/xs93/mmkv/simple/MainActivity.kt
+++ simple/src/main/java/com/github/xs93/mmkv/simple/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.github.xs93.mmkv.simple
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.xs93.mmkv.MMKVOwner
+import com.github.xs93.mmkv.boolean
+import com.tencent.mmkv.MMKV
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MMKVOwner {
+
+    override val mmkv: MMKV
+        get() = MMKV.mmkvWithID("MainActivity")
+
+
+    private var start by mmkv.boolean("start")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        start = true
     }
 }
