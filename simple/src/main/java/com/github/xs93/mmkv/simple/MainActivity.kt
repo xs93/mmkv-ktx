8868cc37b0b308631fa92b15fb85ed8c06a67127
xushuai <466911254@qq.com>
2022-07-29 14:18:13 +0800

    MMKV-KTX Initialize

---
+++ simple/src/main/java/com/github/xs93/mmkv/simple/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.xs93.mmkv.simple
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
